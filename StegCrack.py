67f0ea496e056ff7fa566af1c4fd33acce5193b1
J-Goutham <jgoutham246@gmail.com>
2020-12-02 19:03:44 +0530

    intial commit

--- StegCrack.py
+++ StegCrack.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python
 
+import os, sys
 
-import base64
+file = sys.argv[1]
+list = sys.argv[2]
 
-
-base = """IyEvdXNyL2Jpbi9weXRob24KCmltcG9ydCBvcywgc3lzCgpmaWxlID0gc3lzLmFyZ3ZbMV0KbGlz
-dCA9IHN5cy5hcmd2WzJdCgpvcy5zeXN0ZW0oJ2NvbW1hbmQgLXYgc3RnZWhpZGUgPi9kZXYvbnVs
-bCAyPiYxIHx8IGFwdC1nZXQgaW5zdGFsbCBzdGVnaGlkZSAteSA+L2Rldi9udWxsJykKcHJpbnQg
-KGYiXG5cMDMzWzkxbSBDcmFja2luZyB7ZmlsZX0uLi4uLiIpCgp0cnk6CiAgICBvcy5zeXN0ZW0o
-ZicnJyBcbiBmb3IgaSBpbiAkKGNhdCB7bGlzdH0pOyBkbyBzdGVnaGlkZSAtLWV4dHJhY3QgLXNm
-IHtmaWxlfSAtcCAkaSAyPi9kZXYvbnVsbCAmJiBlY2hvIC1lICIgXDAzM1s5Mm0gUGFzc3dvcmQ6
-ICRpIiAmJiBicmVhazsgZG9uZScnJykKZXhjZXB0IEtleWJvYXJkSW50ZXJydXB0IGFzIGs6CiAg
-IHByaW50ICgnXDAzM1s5Nm0gU3RvcHBlZCEnKQogICBzeXMuZXhpdCgpCgo="""
-
-#print (base)
+print (f"\nCracking {file}.....")
 
 try:
-    exec (base64.b64decode(base))
-except IndexError as i:
-    print ('\033[91m Syntax: python StegCrack.py <File> <Wordlist_to_Crack>')
+    hack = (f''' \n for i in $(cat {list}); do steghide --extract -sf {file} -p $i 2>/dev/null && echo -e "\nPassword: $i" && break; done''')
+    os.system(hack)
+except KeyboardInterrupt as k:
+   print ('\nStopped!')
+   sys.exit()
+
