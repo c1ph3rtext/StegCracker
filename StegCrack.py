8f0d24b1e62cdab47a5001fa871a3fc5863497d1
J-Goutham <73335922+J-Goutham@users.noreply.github.com>
2021-04-17 14:03:53 +0530

    Add files via upload

--- StegCrack.py
+++ StegCrack.py
@@ -3,14 +3,17 @@
 
 import os, sys
 
-file = sys.argv[1]
-list = sys.argv[2]
-
-print (f"\nCracking {file}.....")
-
 try:
+    file = sys.argv[1]
+    list = sys.argv[2]
+    print (f"\nCracking {file}.....")
     hack = (f''' \n for i in $(cat {list}); do steghide --extract -sf {file} -p $i 2>/dev/null && echo -e "\nPassword: $i" && break; done''')
     os.system(hack)
+
+except IndexError as o:
+   print ("Usage: python StegCrack.py file_to_crack passwd_file")
+   sys.exit()
+
 except KeyboardInterrupt as k:
    print ('\nStopped!')
    sys.exit()
